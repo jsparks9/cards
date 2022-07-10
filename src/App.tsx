0effb0614cec9b647617b314bf0fdeb6824c76ea
unknown <josiahsparks78@gmail.com>
2022-07-09 20:27:48 -0500

    additional logic

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,13 @@
 // export default App;
 
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { Card, Button, Typography } from "@mui/material";
 import Draggable from "react-draggable";
-import { useState } from "react";
+import Stack from '@mui/material/Stack';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 interface Card {
   q: string;
@@ -34,33 +36,102 @@ interface Deck {
   memlen: number; // maximum memory length for deck
 }
 
+const api_base = 'https://raw.githubusercontent.com/jsparks9/cards/main/API/';
+const decks = ['SQL','Java','HTMLCSS','JavaScript1','TypeScript','React'];
+const ex = '.json';
+let hasSent = false;
+
 export default function App() {
   const [loadedDecks, setLoadedDecks] = useState<Deck[]>([]); // stores fetched decks
   const [currentDeckInds, setCurrentDeckInds] = useState<number[]>([]); // tracks currently selected decks
   const [currentCards, setCurrentCards] = useState<Card[]>([]); // cards that may be displayed
   const [displayCard, setDisplayCard] = useState<Card>({q:"",a:""} as Card);
 
-  // make some buttons that load in current decks
-  function load(ind:number) {
-    for (let deck of loadedDecks) {
-      if (deck.idx === ind) {
-        return;
-      }
-    } // no matches => fetch the deck
+  const [deckSelection, setDeckSelection] = React.useState(() => [decks[0]]);
 
+  const handleDeckSelection = (
+    event: React.MouseEvent<HTMLElement>,
+    currentSel: string[],
+  ) => {
+    if (currentSel.length) { // enforces at least one selection
+      setDeckSelection(currentSel);
+    }
+  };
+
+  useEffect(() => {
+    
+  }, [deckSelection])
+
+  useEffect(() => {
+    console.log("current selection is " + deckSelection);
+    let inds:number[] = [];
+    for (let entry in deckSelection) {
+      let ind = decks.indexOf(entry)+1;
+      ind && inds.push(ind-1);
+    };
+    //setCurrentDeckInds(inds.sort()); // sort and set
+    if (loadedDecks.length < decks.length) {
+      (inds.filter(item => currentDeckInds.indexOf(item) < 0)).forEach(dif => load(dif));
+    }
+    // check length of "loaded decks" and if not max then 
+    // find what which one(s) has not loaded, then call load(ind)
+  }, [deckSelection])
+
+  // make some buttons that load in current decks
+  async function load(ind:number) {
+    // for (let deck of loadedDecks) {
+    //   if (deck.idx === ind) {
+    //     return;
+    //   }
+    // } // no matches => fetch the deck
+    let resp = await fetch(api_base+decks[ind]+ex);
+    if (Math.floor(resp.status/100) === 2) { // 200 expected
+      let data = await resp.json();
+      injectDeck(ind, await data);
+
+    }
   }
+
+  useEffect(() => {
+
+    console.log(loadedDecks);
+    console.log("length: " + loadedDecks.length)
+  }, [loadedDecks])
+
+  useEffect(() => { 
+    if(!hasSent && !loadedDecks) { // !loadedDecks prevents additional fetches during development
+      hasSent = true; // prevents double request
+      load(0); 
+      //hasSent = false; // not sure about this
+    }
+  }, []) // loads first deck
+
+  function injectDeck(ind:number, data:any) {
+    let deck:Deck = {idx:ind,cards:data as unknown as Card[], memory:[], memlen:0} as Deck;
+    deck.memlen = Math.floor(deck.cards.length / 2);
+    setLoadedDecks([...loadedDecks, deck]);
+  }
+
 
 
   return (
     <div className="App">
-      <div onClick={() => {}}>
-        <p>Button1</p>
+      <div className="Please_delete_me" onClick={() => {}}>
+        <p>Button16</p>
       </div>
-      {/*       
-      <DraggableCard text="Drag" bgColor="#FF9300" />
-      <DraggableCard text="Drop" bgColor="#00A2FF" /> 
-      */}
 
+      <ToggleButtonGroup
+        orientation="vertical"
+        value={deckSelection}
+        onChange={handleDeckSelection}
+        // aria-label="device"
+      >
+        {decks.map((n) =>
+        <ToggleButton key={n} value={n} aria-label={n}>
+          {n}
+        </ToggleButton>
+        )}
+      </ToggleButtonGroup>
     </div>
   );
 }
